ded9967083dac5b6aaef8337de28859d08a4b423
Adam Urbina <adamurbina@gmail.com>
2018-06-13 01:11:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
     end
 
     def execute_transaction
-        if @sender.valid? == false
+        if @sender.valid? == false || amount > @sender.balance
             @status = "rejected"
             return "Transaction rejected. Please check your account balance."
         elsif status == "pending"
